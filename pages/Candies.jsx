bf38a1e21d864ee848e45aea0f65e4e321c0551d
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-12 07:38:50 -0500

    location add to export default and to retrieveCandies in candies.jsx

--- pages/Candies.jsx
+++ pages/Candies.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Page from '../components/Page'
 import Title from '../components/Title'
 
-export default () => {
+export default ({ location }) => {
   const [maunfacturerName, setManufacturerName] = useState('')
   const [manufacturerList, setManufacturerList] = useState({})
   const [candyList, setCandyList] = useState([])
@@ -10,7 +10,7 @@ export default () => {
 
   useEffect(() => {
     async function pullData() {
-      const { details, candies } = await retrieveManufacturers()
+      const { details, candies } = await retrieveCandies(location)
 
       setManufacturerList(details.name)
       setFilteredManufacturerList(details)
