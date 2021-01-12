c39dc5e1155a707212a1b9e89f10877e7c0996ab
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-11 21:17:21 -0500

    css add in title.jsx

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`
+
+const Subtitle = styled.div`
+  font-size: 20px;
+`
 export default () => (
   <>
     <div className="title">Candy Makers</div>
