c9517d396542ae0830846fb61e225ee9b429fd03
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-11 21:16:47 -0500

    css add in page.jsx

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default ({children}) => (
+const Page = styled.div`
+  margin: 60px auto 0;
+  text-align: center;
+  width: 60%;
+  `
+export default ({ children }) => (
   <div className="page">{children}</div>
 )
