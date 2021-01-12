99e659638c245985d946958d2f98d6ac30c7b9f7
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-12 08:01:13 -0500

    candies.js in utils folder getManufacturerNameFromUrl create with location and pathname and a retrieveCandies const

--- utils/candies.js
+++ utils/candies.js
@@ -1,3 +1,9 @@
-export const retrieveCandies = async (locations) => {
+export const getManufacturerNameFromUrl = location => (location &&
+  location.pathname
+  ? location.pathname.split('/candies/').pop()
+  : ''
+)
 
+export const retrieveCandies = async (locations) => {
+  const manufacturerName = getManufacturerNameFromUrl(location)
 }
