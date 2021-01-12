de562fd8b43198f6abcbeddc7259ea41cb478b15
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-12 08:10:59 -0500

    candies.js import fetchCandiesForManufacturer from actions/candies and return add with cleanup

--- utils/candies.js
+++ utils/candies.js
@@ -1,15 +1,25 @@
+import fetchCandiesForManufacturer from '../actions/candies'
+
 export const getManufacturerNameFromUrl = location => (location &&
   location.pathname
   ? location.pathname.split('/candies/').pop()
   : ''
 )
 
-export const retrieveCandies = async (locations) => {
+export const retrieveCandies = async (location) => {
   const manufacturerName = getManufacturerNameFromUrl(location)
 
   if (!manufacturerName) return { details: {}, candies: [] }
 
   const { id, name, country, candies } = await
   fetchCandiesForManufacturer(manufacturerName)
-  
+
+  if (!id || !name || !country || !candies) {
+    return {
+      details: {},
+      candies:
+      [],
+    }
+  }
+  return { candies, details: { id, name, country } }
 }
