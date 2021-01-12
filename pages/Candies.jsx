a472b41d06cc35e46141fdcc2dd79263e6aa25f0
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-11 21:39:34 -0500

    candies.jsx update with useState

--- pages/Candies.jsx
+++ pages/Candies.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Page from '../components/Page'
 import Title from '../components/Title'
 
-export default () => (
-  <Page>
-    <Title />
-  </Page>
-)
+export default () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [manufacturerList, setManufacturerList] = useState({})
+  const [filteredManufacturerList, setFilteredManufacturerList] = useState([])
+
+  return (
+    <Page>
+      <Title />
+    </Page >
+  )
+}
