edcaa058927471ad82b7766d698e75ae4ee49659
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-11 21:04:29 -0500

    import Title to error page and put in self closing Title div

--- pages/Error.jsx
+++ pages/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import Title from '../components/Title'
 
 export default () => (
   <div className="page">
+    <Title />
     <div>Sorry, unable to find the page you are looking for</div>
   </div>
 )
