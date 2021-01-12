cfed2a50b365573df1f814c491452c3ce7ebf8d7
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-12 07:29:12 -0500

    candyList useState add retrieveCandies details add. Paramater change to details.name, details and candies

--- pages/Candies.jsx
+++ pages/Candies.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Page from '../components/Page'
 import Title from '../components/Title'
 
 export default () => {
-  const [searchTerm, setSearchTerm] = useState('')
+  const [maunfacturerName, setManufacturerName] = useState('')
   const [manufacturerList, setManufacturerList] = useState({})
+  const [candyList, setCandyList] = useState([])
   const [filteredManufacturerList, setFilteredManufacturerList] = useState([])
+
+  useEffect(() => {
+    async function pullData() {
+      const { details, candies } = await retrieveManufacturers()
+
+      setManufacturerList(details.name)
+      setFilteredManufacturerList(details)
+      setCandyList(candies)
+    }
+    pullData()
+  }, [])
 
   return (
     <Page>
