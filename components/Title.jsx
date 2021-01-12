52d78531553e91f4496851079548eb599ede5602
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-11 21:02:43 -0500

    cut title and subtitle from error in pages folder and add to title

---
+++ components/Title.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <>
+    <div className="title">Candy Makers</div>
+    <div className="subtitle">A searchable list of all your favorite manufacturers of candies</div>
+  </>
+)
---
+++ pages/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+  <div className="page">
+    <div>Sorry, unable to find the page you are looking for</div>
+  </div>
+)
