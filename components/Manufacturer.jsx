defb1b175881fc147a1b278d22ac5df18d4e98ca
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-11 21:18:22 -0500

    css add in team.jsx

--- components/Manufacturer.jsx
+++ components/Manufacturer.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Team = styled.div`
+  font-size: 20px;
+  margin-bottom: 10px;
+`
 
 export default ({ id, name, country }) => (
   <div key={id} className="manufacturer">{`${name} (${country})`}</div>
